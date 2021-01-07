ad21900378bf73cb57118c113a25d4698a89687d
simonkimi <805757448@qq.com>
2021-01-08 00:56:08 +0800

    添加画廊的模型

--- lib/models/gallery_model.dart
+++ lib/models/gallery_model.dart
@@ -1,3 +1,11 @@
 class GalleryModel {
-  
+  final String parent;
+  final String visible;
+  final String fileSize;
+  final String favorited;
+  final Map<String, List<String>> tags;
+
+  GalleryModel(
+      {this.parent, this.visible, this.fileSize, this.favorited, this.tags});
+
 }
